407f3374faa7b2466eb61db9829f081d31312096
Rohil Saraf <rs2685@cornell.edu>
2026-09-16 18:11:32 -0400

    complete screen clock

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -64,7 +64,9 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    OUTPUT = time.strftime("%m/%d/%Y %H:%M:%S")
+    y = top
+    draw.text((x, y), OUTPUT, font=font, fill="#FFFFFF")
 
     # Display image.
     disp.image(image, rotation)
